3c081cd2e4d0c043475648b56f1da6487cc61ec9
Tee Kai Yang <tkaiyang2005@gmail.com>
2025-11-19 11:35:16 +0800

    Delete util directory

--- util/Utils.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class Utils {
-    public static String[] safeSplit(String line, char sep){
-        // simple split that doesn't attempt to handle quoted commas for simplicity
-        return line.split(String.valueOf(sep));
-    }
-}
